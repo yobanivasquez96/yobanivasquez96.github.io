eb2b3cbdada3049d70203314b2ed566d4ab3a953
yobanivasquez96 <93278239+yobanivasquez96@users.noreply.github.com>
2021-11-29 13:17:23 -0700

    Add files via upload

---
+++ forwardfitness/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name:Yobani Vasquez
+    File Name: script.js
+    Date: 11/29/2021
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});

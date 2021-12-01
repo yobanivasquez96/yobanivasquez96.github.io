151fd5b39c8714c685bb12c916a74c9d51f324e0
yobanivasquez96 <93278239+yobanivasquez96@users.noreply.github.com>
2021-12-01 12:14:23 -0700

    Add files via upload

---
+++ wildrescuebootstrap/scripts/script.js
@@ -0,0 +1,13 @@
+/*
+    Student Name:Yobani Vasquez
+    File Name: script.js
+    Date: 12/1/2021
+*/
+
+//jQuery for hero image to consume header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
+
+
+
